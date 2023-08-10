fd0202b711d189516406cea9b5c1c8d66f614c9d
Jonas Söderberg <imevul@gmail.com>
2023-08-10 21:29:19 +0200

    Added fix to only highlight valid items.

--- AtlasLoot/Core/AtlasLoot.lua
+++ AtlasLoot/Core/AtlasLoot.lua
@@ -28,6 +28,90 @@ AtlasLoot_RefreshQuickLookButtons()
 AtlasLoot_AddTooltip(frameb, tooltiptext)
 ]]
 
+-- Credit: Synastria - meh321
+local function IsArmorTypeValid(itemType, itemSubType, itemLevel)
+    if not itemType or itemType ~= "Armor" or not itemSubType then return true end
+    local ctype = 0
+    if itemSubType == "Cloth" then ctype = 1 end
+    if itemSubType == "Leather" then ctype = 2 end
+    if itemSubType == "Mail" then ctype = 3 end
+    if itemSubType == "Plate" then ctype = 4 end
+    if ctype == 0 then return true end
+    local mtype = 0
+    local mtype2 = 0
+    local _, mclass = UnitClass("player")
+    if mclass == "WARRIOR" or mclass == "PALADIN" or mclass == "DEATHKNIGHT" then
+        mtype = 4
+        if itemLevel < 45 then mtype2 = 3 end
+    elseif mclass == "DRUID" or mclass == "ROGUE" then
+        mtype = 2
+    elseif mclass == "HUNTER" or mclass == "SHAMAN" then
+        if itemLevel < 45 then mtype = 2 else mtype = 3 end
+    else -- warlock, mage, priest
+        mtype = 1
+    end
+    if mtype ~= ctype and (mtype2 == 0 or mtype2 ~= ctype) then
+        return false
+    end
+    return true
+end
+
+-- Credit: Synastria - meh321
+local function CheckItemValid(itemId)
+    if not itemId or itemId <= 0 or ItemAttuneSkip == nil or not ItemAttuneHas then return 0 end
+    local _, _, _, itemLevel, itemMinLevel, itemType, itemSubType, _, itemEquipLoc = GetItemInfo("item:" .. itemId)
+    if not itemEquipLoc or string.len(itemEquipLoc) == 0 then return 0 end
+    if itemEquipLoc == "INVTYPE_NON_EQUIP" then return 0 end
+    if itemEquipLoc == "INVTYPE_BODY" then return 0 end
+    if itemEquipLoc == "INVTYPE_BAG" then return 0 end
+    if itemEquipLoc == "INVTYPE_TABARD" then return 0 end
+    if itemEquipLoc == "INVTYPE_AMMO" then return 0 end
+    if itemEquipLoc == "INVTYPE_QUIVER" then return 0 end
+    
+    -- show N/A
+    if ItemAttuneSkip[itemId] then return -5 end
+    if itemEquipLoc ~= "INVTYPE_CLOAK" and not SynastriaCoreLib.IsArmorTypeValid(itemType, itemSubType, itemLevel) then return -2 end
+    if itemEquipLoc == "INVTYPE_2HWEAPON" then return -4 end
+    if itemEquipLoc == "INVTYPE_WEAPON" then return -4 end
+    if itemEquipLoc == "INVTYPE_WEAPONMAINHAND" then return -4 end
+    if itemEquipLoc == "INVTYPE_WEAPONOFFHAND" then return -4 end
+    if itemEquipLoc == "INVTYPE_SHIELD" then
+        local _, mclass = UnitClass("player")
+        if mclass ~= "WARRIOR" and mclass ~= "PALADIN" and mclass ~= "SHAMAN" then return -6 end
+    end
+    if itemType == "Weapon" then
+        if itemSubType == "Thrown" then
+            local _, mclass = UnitClass("player")
+            if mclass ~= "WARRIOR" and mclass ~= "ROGUE" then return -6 end
+        elseif itemSubType == "Guns" or itemSubType == "Bows" or itemSubType == "Crossbows" then
+            local _, mclass = UnitClass("player")
+            if mclass ~= "WARRIOR" and mclass ~= "ROGUE" and mclass ~= "HUNTER" then return -6 end
+        elseif itemSubType == "Wands" then
+            local _, mclass = UnitClass("player")
+            if mclass ~= "MAGE" and mclass ~= "PRIEST" and mclass ~= "WARLOCK" then return -6 end
+        end
+    end
+    if itemEquipLoc == "INVTYPE_RELIC" then
+        if itemSubType == "Librams" then
+            local _, mclass = UnitClass("player")
+            if mclass ~= "PALADIN" then return -6 end
+        elseif itemSubType == "Idols" then
+            local _, mclass = UnitClass("player")
+            if mclass ~= "DRUID" then return -6 end
+        elseif itemSubType == "Totems" then
+            local _, mclass = UnitClass("player")
+            if mclass ~= "SHAMAN" then return -6 end
+        elseif itemSubType == "Sigils" then
+            local _, mclass = UnitClass("player")
+            if mclass ~= "DEATHKNIGHT" then return -6 end
+        end
+    end
+    if ItemAttuneStatless[itemId] then return -3 end
+    
+    -- show valid
+    return 1
+end
+
 AtlasLoot = LibStub("AceAddon-3.0"):NewAddon("AtlasLoot");
 
 --Instance required libraries
@@ -733,7 +817,7 @@ function AtlasLoot_ShowItemsFrame(dataID, dataSource, boss, pFrame)
                 end
 
 				--Highlight unattuned items
-				if dataSource[dataID][i][2] ~= "" and dataSource[dataID][i][2] ~= 0 and ItemAttuneHas and (ItemAttuneHas[dataSource[dataID][i][2]] or 0) < 100 then
+				if dataSource[dataID][i][2] ~= "" and dataSource[dataID][i][2] ~= 0 and ItemAttuneHas and (ItemAttuneHas[dataSource[dataID][i][2]] or 0) < 100 and CheckItemValid(dataSource[dataID][i][2]) > 0 then
 					text = "!! "..text;
 				end
 
