b1234f5d443d0c367060b3e9db5a2606f0e91cd2
Lubricated <jimmy.olsson962@gmail.com>
2024-03-15 23:19:29 +0000

    Add 5 new mythics (WIP, several items in question)

--- src/AtlasLoot/TableRegister/loottables.en.lua
+++ src/AtlasLoot/TableRegister/loottables.en.lua
@@ -1824,3 +1824,25 @@ end
 	AtlasLoot_TableNames["MythicTKArcDalliah"] = { BabbleBoss["Dalliah the Doomsayer"], "AtlasLootMythic" };
 	AtlasLoot_TableNames["MythicTKArcScryer"] = { BabbleBoss["Wrath-Scryer Soccothrates"], "AtlasLootMythic" };
 	AtlasLoot_TableNames["MythicTKArcHarbinger"] = { BabbleBoss["Harbinger Skyriss"], "AtlasLootMythic" };
+
+	AtlasLoot_TableNames["MythicCoTMorassDeja"] = { BabbleBoss["Chrono Lord Deja"], "AtlasLootBurningCrusade" };
+	AtlasLoot_TableNames["MythicCoTMorassTemporus"] = { BabbleBoss["Temporus"], "AtlasLootBurningCrusade" };
+	AtlasLoot_TableNames["MythicCoTMorassAeonus"] = { BabbleBoss["Aeonus"], "AtlasLootBurningCrusade" };
+
+	AtlasLoot_TableNames["MythicAuchCryptsShirrak"] = { BabbleBoss["Shirrak the Dead Watcher"], "AtlasLootBurningCrusade" };
+	AtlasLoot_TableNames["MythicAuchCryptsExarch"] = { BabbleBoss["Exarch Maladaar"], "AtlasLootBurningCrusade" };
+	AtlasLoot_TableNames["MythicAuchCryptsAvatar"] = { AL["Avatar of the Martyred"], "AtlasLootBurningCrusade" };
+
+	AtlasLoot_TableNames["MythicAuchSethekkDarkweaver"] = { BabbleBoss["Darkweaver Syth"], "AtlasLootBurningCrusade" };
+	AtlasLoot_TableNames["MythicAuchSethekkRavenGod"] = { BabbleBoss["Anzu"].." ("..AL["Heroic"]..")", "AtlasLootBurningCrusade" };
+	AtlasLoot_TableNames["MythicAuchSethekkTalonKing"] = { BabbleBoss["Talon King Ikiss"], "AtlasLootBurningCrusade" };
+
+	AtlasLoot_TableNames["MythicTKBotSarannis"] = { BabbleBoss["Commander Sarannis"], "AtlasLootBurningCrusade" };
+	AtlasLoot_TableNames["MythicTKBotFreywinn"] = { BabbleBoss["High Botanist Freywinn"], "AtlasLootBurningCrusade" };
+	AtlasLoot_TableNames["MythicTKBotThorngrin"] = { BabbleBoss["Thorngrin the Tender"], "AtlasLootBurningCrusade" };
+	AtlasLoot_TableNames["MythicTKBotLaj"] = { BabbleBoss["Laj"], "AtlasLootBurningCrusade" };
+	AtlasLoot_TableNames["MythicTKBotSplinterHEROIC"] = { BabbleBoss["Warp Splinter"].." ("..AL["Heroic"]..")", "AtlasLootBurningCrusade" };
+
+	AtlasLoot_TableNames["MythicCoTHillsbradDrake"] = { BabbleBoss["Lieutenant Drake"], "AtlasLootBurningCrusade" };
+	AtlasLoot_TableNames["MythicCoTHillsbradSkarloc"] = { BabbleBoss["Captain Skarloc"], "AtlasLootBurningCrusade" };
+	AtlasLoot_TableNames["MythicCoTHillsbradHunterHEROIC"] = { BabbleBoss["Epoch Hunter"].." ("..AL["Heroic"]..")", "AtlasLootBurningCrusade" };
